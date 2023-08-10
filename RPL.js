510230c4216ff02849f73d0e82ae4a3c8474e731
RehanDiasTEST <141944367+RehanDiasTEST@users.noreply.github.com>
2023-08-10 22:49:32 +0700

    Add files via upload

---
+++ RPL.js
@@ -0,0 +1,55 @@
+// Impor modul yang dibutuhkan
+const fs = require('fs');
+const esprima = require('esprima');
+const estraverse = require('estraverse');
+
+// Definisikan fungsi untuk menghitung kompleksitas siklomatik dari kode sumber yang diberikan
+function hitungKompleksitasSiklomatik(kodeSumber) {
+  // Parse kode sumber menjadi AST
+  const ast = esprima.parseScript(kodeSumber);
+
+  // Inisialisasi variabel untuk menyimpan jumlah edge, node, dan komponen terhubung dalam graf alur kontrol
+  let edge = 0;
+  let node = 0;
+  let komponenTerhubung = 1;
+
+  // Lakukan penjelajahan pada AST dan perbarui variabel sesuai kondisi
+  estraverse.traverse(ast, {
+    enter: function (node) {
+      // Tambahkan satu ke jumlah node untuk setiap node dalam AST
+      node++;
+
+      // Tambahkan satu ke jumlah edge untuk setiap node yang mewakili struktur kontrol, seperti if, switch, for, while, dll.
+      if (node.type === 'IfStatement') {
+        edge++;
+        // Jika node memiliki cabang alternatif (else atau else if), tambahkan satu lagi ke jumlah edge
+        if (node.alternate) {
+          edge++;
+        }
+      } else if (node.type === 'SwitchStatement') {
+        // Tambahkan jumlah edge sebanyak jumlah kasus dalam pernyataan switch
+        edge += node.cases.length;
+      } else if (node.type === 'ForStatement' || node.type === 'ForInStatement' || node.type === 'ForOfStatement' || node.type === 'WhileStatement' || node.type === 'DoWhileStatement') {
+        edge++;
+      }
+    },
+    leave: function (node) {
+      // Jika node adalah deklarasi atau ekspresi fungsi, tambahkan satu ke jumlah komponen terhubung
+      if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression') {
+        komponenTerhubung++;
+      }
+    }
+  });
+
+  // Hitung dan kembalikan kompleksitas siklomatik menggunakan rumus
+  return edge - node + 2 * komponenTerhubung;
+}
+
+// Baca kode sumber dari sebuah berkas
+const kodeSumber = fs.readFileSync('./source.js', 'utf8');
+
+// Panggil fungsi untuk menghitung kompleksitas siklomatik
+const kompleksitas = hitungKompleksitasSiklomatik(kodeSumber);
+
+// Cetak hasilnya
+console.log('Kompleksitas siklomatik:', kompleksitas);
---
+++ source.js
@@ -0,0 +1,94 @@
+const data = require('./jadwal.json'); // Ganti dengan path yang sesuai
+
+const weekdays = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
+const saturdayTimes = ['07:00 - 09:29', '09:30 - 11:59', '12:00 - 14:29'];
+
+// Mengacak urutan elemen dalam array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Mendapatkan mata kuliah unik berdasarkan hari
+function getUniqueCoursesForDay(day, currentCourses) {
+  const availableCourses = data.mata_kuliah.filter(course => course.hari === day && !currentCourses.includes(course.nama));
+  return availableCourses;
+}
+
+// Menghasilkan kemungkinan jadwal
+function generateRandomSchedule() {
+  const selectedCourses = [];
+  const schedule = [];
+
+  const shuffledWeekdays = shuffleArray([...weekdays]);
+
+  shuffledWeekdays.forEach(day => {
+    const availableCourses = getUniqueCoursesForDay(day, selectedCourses);
+    if (availableCourses.length > 0) {
+      const selectedCourse = availableCourses[Math.floor(Math.random() * availableCourses.length)];
+      selectedCourses.push(selectedCourse.nama);
+      schedule.push({
+        Hari: day,
+        Mata_Kuliah: selectedCourse.nama,
+        Jam: selectedCourse.jam,
+        Ruang: selectedCourse.ruang
+      });
+    }
+  });
+
+  const saturdayCoursesAvailable = data.mata_kuliah.filter(course => course.hari === 'Sabtu');
+
+  saturdayTimes.forEach(time => {
+    const availableCourses = saturdayCoursesAvailable.filter(course => !selectedCourses.includes(course.nama) && course.jam === time);
+    if (availableCourses.length > 0) {
+      const selectedCourse = availableCourses[Math.floor(Math.random() * availableCourses.length)];
+      selectedCourses.push(selectedCourse.nama);
+      schedule.push({
+        Hari: 'Sabtu',
+        Mata_Kuliah: selectedCourse.nama,
+        Jam: selectedCourse.jam,
+        Ruang: selectedCourse.ruang
+      });
+    }
+  });
+
+  return schedule;
+}
+
+// Menghasilkan berbagai kemungkinan jadwal yang unik
+function generateUniqueSchedules(numSchedules) {
+  const uniqueSchedules = new Set();
+  
+  const dayOrder = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+  while (uniqueSchedules.size < numSchedules) {
+    const schedule = generateRandomSchedule();
+    const sortedSchedule = schedule.sort((a, b) => {
+      const dayA = dayOrder.indexOf(a.Hari);
+      const dayB = dayOrder.indexOf(b.Hari);
+      if (dayA === dayB) {
+        const timeA = a.Jam.split(' - ')[0];
+        const timeB = b.Jam.split(' - ')[0];
+        return timeA.localeCompare(timeB);
+      }
+      return dayA - dayB;
+    });
+    uniqueSchedules.add(JSON.stringify(sortedSchedule));
+  }
+
+  return Array.from(uniqueSchedules).map(scheduleStr => JSON.parse(scheduleStr));
+}
+
+// Menghasilkan dan mencetak berbagai kemungkinan jadwal yang unik
+const numSchedules = 5; // Ubah sesuai dengan jumlah kemungkinan yang Anda inginkan
+const possibleSchedules = generateUniqueSchedules(numSchedules);
+
+console.log(`Berbagai Kemungkinan Jadwal (${numSchedules} Kemungkinan):\n`);
+possibleSchedules.forEach((schedule, index) => {
+  console.log(`Kemungkinan ${index + 1}:`);
+  console.table(schedule);
+  console.log('\n');
+});
