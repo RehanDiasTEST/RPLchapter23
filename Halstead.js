b7c8f8f7f942d43c8b502d7e1643252926358855
RehanDiasTEST <141944367+RehanDiasTEST@users.noreply.github.com>
2023-08-11 02:19:17 +0700

    Update Halstead.js

--- Halstead.js
+++ Halstead.js
@@ -1,9 +1,52 @@
 // Membaca kode sumber dari file source.js
-const fs = require('fs');
-const sourceCode = fs.readFileSync('source.js', 'utf-8');
+const fs = require("fs");
+const sourceCode = fs.readFileSync("source.js", "utf-8");
 
 // Mendefinisikan operator dan operand yang valid dalam JavaScript
-const operators = ['+', '-', '*', '/', '%', '=', '==', '===', '!=', '!==', '<', '<=', '>', '>=', '&&', '||', '!', '?', ':', '.', ',', ';', '{', '}', '[', ']', '(', ')', 'function', 'if', 'else', 'for', 'while', 'do', 'switch', 'case', 'break', 'default', 'return', 'const', 'let', 'var'];
+const operators = [
+	"+",
+	"-",
+	"*",
+	"/",
+	"%",
+	"=",
+	"==",
+	"===",
+	"!=",
+	"!==",
+	"<",
+	"<=",
+	">",
+	">=",
+	"&&",
+	"||",
+	"!",
+	"?",
+	":",
+	".",
+	",",
+	";",
+	"{",
+	"}",
+	"[",
+	"]",
+	"(",
+	")",
+	"function",
+	"if",
+	"else",
+	"for",
+	"while",
+	"do",
+	"switch",
+	"case",
+	"break",
+	"default",
+	"return",
+	"const",
+	"let",
+	"var",
+];
 const operands = /[a-zA-Z_$][a-zA-Z0-9_$]*/g;
 
 // Menghitung jumlah operator dan operand yang berbeda dan total
@@ -12,66 +55,66 @@ const distinctOperands = new Set();
 const totalOperators = [];
 const totalOperands = [];
 
-let currentToken = '';
+let currentToken = "";
 let isString = false;
 let isComment = false;
 
 for (let i = 0; i < sourceCode.length; i++) {
-  const char = sourceCode[i];
+	const char = sourceCode[i];
 
-  // Melewati karakter dalam string atau komentar
-  if (isString || isComment) {
-    if (char === '"' || char === "'") {
-      isString = !isString;
-    }
-    if (char === '\n') {
-      isComment = false;
-    }
-    continue;
-  }
+	// Melewati karakter dalam string atau komentar
+	if (isString || isComment) {
+		if (char === '"' || char === "'") {
+			isString = !isString;
+		}
+		if (char === "\n") {
+			isComment = false;
+		}
+		continue;
+	}
 
-  // Memeriksa apakah karakter adalah operator
-  if (operators.includes(char)) {
-    // Menambahkan token sebelumnya sebagai operand jika ada
-    if (currentToken) {
-      distinctOperands.add(currentToken);
-      totalOperands.push(currentToken);
-      currentToken = '';
-    }
+	// Memeriksa apakah karakter adalah operator
+	if (operators.includes(char)) {
+		// Menambahkan token sebelumnya sebagai operand jika ada
+		if (currentToken) {
+			distinctOperands.add(currentToken);
+			totalOperands.push(currentToken);
+			currentToken = "";
+		}
 
-    // Memeriksa apakah karakter berikutnya adalah bagian dari operator
-    const nextChar = sourceCode[i + 1];
-    const twoCharOperator = char + nextChar;
-    if (operators.includes(twoCharOperator)) {
-      // Menambahkan operator dua karakter sebagai operator
-      distinctOperators.add(twoCharOperator);
-      totalOperators.push(twoCharOperator);
-      i++;
-    } else {
-      // Menambahkan operator satu karakter sebagai operator
-      distinctOperators.add(char);
-      totalOperators.push(char);
-    }
+		// Memeriksa apakah karakter berikutnya adalah bagian dari operator
+		const nextChar = sourceCode[i + 1];
+		const twoCharOperator = char + nextChar;
+		if (operators.includes(twoCharOperator)) {
+			// Menambahkan operator dua karakter sebagai operator
+			distinctOperators.add(twoCharOperator);
+			totalOperators.push(twoCharOperator);
+			i++;
+		} else {
+			// Menambahkan operator satu karakter sebagai operator
+			distinctOperators.add(char);
+			totalOperators.push(char);
+		}
 
-    // Memeriksa apakah karakter adalah awal dari string atau komentar
-  } else if (char === '"' || char === "'") {
-    isString = true;
-  } else if (char === '/' && sourceCode[i + 1] === '/') {
-    isComment = true;
+		// Memeriksa apakah karakter adalah awal dari string atau komentar
+	} else if (char === '"' || char === "'") {
+		isString = true;
+	} else if (char === "/" && sourceCode[i + 1] === "/") {
+		isComment = true;
 
-    // Memeriksa apakah karakter adalah bagian dari operand
-  } else if (char.match(/[a-zA-Z0-9_$]/)) {
-    currentToken += char;
+		// Memeriksa apakah karakter adalah bagian dari operand
+	} else if (char.match(/[a-zA-Z0-9_$]/)) {
+		currentToken += char;
 
-    // Memeriksa apakah karakter adalah spasi atau baris baru
-  } else if (char.match(/\s/)) {
-    // Menambahkan token sebelumnya sebagai operand jika ada
-    if (currentToken) {
-      distinctOperands.add(currentToken);
-      totalOperands.push(currentToken);
-      currentToken = '';
-    }
-  }
+		// Memeriksa apakah karakter adalah spasi atau baris baru
+	} else if (char.match(/\s/)) {
+		// Menambahkan token sebelumnya sebagai operand jika ada
+		if (currentToken) {
+			distinctOperands.add(currentToken);
+			totalOperands.push(currentToken);
+			currentToken = "";
+		}
+	}
 }
 
 // Menghitung metrik Halstead berdasarkan jumlah operator dan operand yang berbeda dan total
@@ -96,20 +139,20 @@ const B = Math.pow(E, 0.667) / 3000; // Jumlah bug yang dikirimkan
 
 // Membuat objek yang berisi metrik Halstead dan nilai-nilainya
 const halsteadMetrics = {
-  n1,
-  n2,
-  N1,
-  N2,
-  n,
-  N,
-  V,
-  D,
-  L,
-  E,
-  T,
-  B,
+	n1,
+	n2,
+	N1,
+	N2,
+	n,
+	N,
+	V,
+	D,
+	L,
+	E,
+	T,
+	B,
 };
 
 // Mencetak hasil analisis Halstead dalam bentuk tabel
-console.log('Hasil Analisis Halstead:\n');
+console.log("Hasil Analisis Halstead:\n");
 console.table(halsteadMetrics);
